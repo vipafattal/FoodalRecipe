3a774964e4b8f0539d6c6e37b2c7f23e97c9c688
vipafattal <vipafattal@gmail.com>
2019-03-15 18:52:41 +0200

    Fixing SocketTimeoutException

---
+++ app/src/main/java/com/magenta/foodalrecipe/framework/di/AppComponent.kt
@@ -0,0 +1,13 @@
+package com.magenta.foodalrecipe.framework.di
+
+/**
+ * Created by ${User} on ${Date}
+ */
+
+/*
+@Singleton
+@Component(modules = [AppModule::class])
+interface AppComponent {
+
+    fun inject(mainContext: AppMainContext)
+}*/
---
+++ app/src/main/java/com/magenta/foodalrecipe/framework/di/AppModule.kt
@@ -0,0 +1,28 @@
+package com.magenta.foodalrecipe.framework.di
+
+/**
+ * Created by ${User} on ${Date}
+ */
+
+
+/*
+@Module
+open class AppModule(val dao: RecipeDatabaseDao,val food2ForkAPI: Food2ForkAPI) {
+
+    @Provides
+    fun coroutineIO(): CoroutineScope = CoroutineScope(Dispatchers.IO)
+
+    @Singleton
+    @Provides
+    fun provideCache(): Food2ForkLocalCache = Food2ForkLocalCache(dao, coroutineIO())
+
+    @Singleton
+    @Provides
+    fun provideFood2ForkRepo() = Food2ForkRepository(food2ForkAPI, provideCache())
+
+    @Singleton
+    @Provides
+    fun provideViewModelFactory(): ViewModelProvider.Factory {
+        return ViewModelFactory(provideFood2ForkRepo())
+    }
+}*/
